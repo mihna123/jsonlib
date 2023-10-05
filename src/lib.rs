dea53960f654779be7d785994d65c54efffec313
mihailo <mihailonvojinovic@gmail.com>
2023-10-05 10:59:56 +0200

    Add Value enum

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,17 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub mod parser;
+
+use std::collections::HashMap;
+
+pub enum Value {
+    Object(HashMap<String, Value>),
+    Array(Vec<Value>),
+    String(String),
+    Number(Number),
+    Bool(bool),
+    Null
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub enum Number {
+    Integer(i32),
+    Real(f64)
 }
