4f25d38b97b22b272ca7e89f29003846731e6af4
mihailo <mihailonvojinovic@gmail.com>
2023-10-06 01:20:37 +0200

    Make tokenizer tokenize more tokens

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -25,17 +25,114 @@ impl Tokenizer {
             }
 
             match c {
-                '"' => {/*Handle string token*/}
+                '"' => self.handle_string(&mut res),
                 '{' => res.push(Token::OpenCurlyBrace),
                 '}' => res.push(Token::ClosedCurlyBrace),
                 ':' => res.push(Token::Colon),
-                ' ' => {/*Handle whitespace*/}
-                't' => {/*Handle posible true token*/}
-                'f' => {/*Handle posible false token*/}
+                't' => self.handle_true(&mut res),
+                'f' => self.handle_false(&mut res),
                 '0' ..= '9' => {/*Handle posible number token*/}
                 _ => {}
             }
         }
         res
+    }
+
+    fn handle_true(&mut self, tokens: &mut Vec<Token>) {
+        //TODO: fix the unwraping
+        let mut next = self.stream.get_char().unwrap();
+        if next != 'r' {
+            return;
+        }
+        next = self.stream.get_char().unwrap();
+        if next != 'u' {
+            return;
+        }
+        next = self.stream.get_char().unwrap();
+        if next != 'e' {
+            return;
+        }
+
+        tokens.push(Token::True);
+    }
+
+    fn handle_false(&mut self, tokens: &mut Vec<Token>) {
+        //TODO: fix the unwraping
+        let mut next = self.stream.get_char().unwrap();
+        if next != 'a' {
+            return;
+        }
+        next = self.stream.get_char().unwrap();
+        if next != 'l' {
+            return;
+        }
+        next = self.stream.get_char().unwrap();
+        if next != 's' {
+            return;
+        }
+        next = self.stream.get_char().unwrap();
+        if next != 'e' {
+            return;
+        }
+
+        tokens.push(Token::False);
+    }
+
+    fn handle_string(&mut self, tokens: &mut Vec<Token>) {
+        let mut string_val = String::new();
+        loop {
+            let c: char;
+            match self.stream.get_char() {
+                Some(character) => { c = character }
+                None => break
+            }
+            match c {
+                '"' => break,
+                _ => { string_val.push(c) }
+            }
+        }
+        tokens.push(Token::String { value: string_val });
+    }
+}
+
+#[cfg(test)]
+pub mod test {
+    use super::*;
+    
+    #[test]
+    fn test_simple_string() {
+        let mut tokenizer = Tokenizer::new("\"Hello World!\"");
+        let tokens = tokenizer.tokenize();
+        assert_eq!(tokens, vec![Token::String {value: "Hello World!".to_string()}])
+    }
+
+    #[test]
+    fn test_simple_tokens() {
+        let mut tokenizer = Tokenizer::new("{\"hi\"}");
+        let tokens = tokenizer.tokenize();
+        assert_eq!(tokens,
+                   vec![Token::OpenCurlyBrace,
+                        Token::String {value: "hi".to_string()},
+                        Token::ClosedCurlyBrace]);
+    }
+
+    #[test]
+    fn test_true_token() {
+        let mut tokenizer = Tokenizer::new("\"true\":true");
+        let tokens = tokenizer.tokenize();
+        assert_eq!(tokens,
+                   vec![Token::String {value: "true".to_string()},
+                        Token::Colon,
+                        Token::True])
+    }
+
+    #[test]
+    fn test_false_token() {
+        let mut tokenizer = Tokenizer::new("\"false\":false");
+        let tokens = tokenizer.tokenize();
+        assert_eq!(tokens,
+                   vec![Token::String {value: "false".to_string()},
+                        Token::Colon,
+                        Token::False])
     }
 }
