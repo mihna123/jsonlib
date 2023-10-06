68942f0df3c09ff26dce193b623b40a9f3cbebc7
mihailo <mihailonvojinovic@gmail.com>
2023-10-06 18:05:43 +0200

    Move value to its own module

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,3 @@
 pub mod parser;
 pub mod tokenizer;
-
-use std::collections::HashMap;
-
-pub enum Value {
-    Object(HashMap<String, Value>),
-    Array(Vec<Value>),
-    String(String),
-    Number(f64),
-    Bool(bool),
-    Null
-}
+pub mod value;
---
+++ src/value.rs
@@ -0,0 +1,11 @@
+use std::collections::HashMap;
+
+#[derive(PartialEq, Debug)]
+pub enum Value {
+    Object(HashMap<String, Value>),
+    Array(Vec<Value>),
+    String(String),
+    Number(f64),
+    Bool(bool),
+    Null
+}
