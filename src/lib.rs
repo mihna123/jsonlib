fb4dbc509fccfb424704b52939ac686e056310e8
mihailo <mihailonvojinovic@gmail.com>
2023-10-12 12:04:47 +0200

    Make parse api public

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,15 @@
-pub mod parser;
-pub mod tokenizer;
-pub mod value;
+mod parser;
+mod tokenizer;
+mod value;
+
+use value::Value;
+use parser::Parser;
+use std::error::Error;
+
+pub fn parse(input: &str) -> Result<Value, Box<dyn Error>> {
+    let mut parser = Parser::new();
+    parser.read_into_stream(input);
+    parser.parse_obj()
+}
+
+
