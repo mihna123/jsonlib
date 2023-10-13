086313e09e8276ee4c9f7a8e6487eeec2e08cea4
mihailo <mihailonvojinovic@gmail.com>
2023-10-13 12:57:17 +0200

    Add another parsing test

--- tests/parse_tests.rs
+++ tests/parse_tests.rs
@@ -13,6 +13,20 @@ fn test_parsing_01() {
     } else {
         panic!("Parsed value was not object");
     }
-    
-    
+}
+
+#[test]
+fn test_parsing_02() {
+    let data = fs::read_to_string("tests/data.json").expect("Should be able to open");
+    let obj = jsonlib::parse(data.as_str()).expect("should parse no problem");
+    if let Value::Object(o) = &obj {
+        if let Value::Object(glossary) = &o["glossary"] {
+            if let Value::Object(gloss_div) = &glossary["GlossDiv"] {
+                assert_eq!(gloss_div["title"], Value::String("S".to_string()));
+            }
+        }
+    } else {
+        panic!("Value is not an object!");
+    }
+
 }
